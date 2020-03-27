30dff404391a582ec42edf07f5dea69f5caa4cc8
Max Freudenberg <max.freudenberg@protonmail.com>
2020-03-27 17:06:53 +0100

    Correct make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -2,4 +2,4 @@ using Documenter, LibASICamera
 
 makedocs(sitename="LibASICamera API")
 
-deploydocs(repo = "https://github.com/AlfTetzlaff/LibASICamera.jl.git")
+deploydocs(repo = "github.com/AlfTetzlaff/LibASICamera.jl.git")
